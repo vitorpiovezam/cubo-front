0d9f5df8f814e247175ee162875824d0c4d3355b
Vitor Gonçalves Piovezam <vitorpiovezam@yandex.com>
2019-06-26 21:50:16 -0300

    set optional attributes

--- src/app/pages/users/shared/user.model.ts
+++ src/app/pages/users/shared/user.model.ts
@@ -1,11 +1,13 @@
 export class User {
-    firstName: string;
-    lastName: string;
-    participation: number;
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    participation?: number;
 
-    constructor(firstName: string, lastName: string, participation: number) {
+    constructor(firstName?: string, lastName?: string, participation?: number) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.participation = participation;
     }
+
 }
